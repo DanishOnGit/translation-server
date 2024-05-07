97934f04714b40df99cdfebb4aa45bb733d4099a
danishgrappus <127500304+danishgrappus@users.noreply.github.com>
2024-05-08 02:48:04 +0530

    Add endpoints

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
 // Middleware to parse JSON body
 app.use(bodyParser.json());
 app.use(cors());
+app.get('/',(req,res)=>{
+    res.json({msg:'hii'})
+})
 // POST endpoint to receive translation JSON
 app.post('/getTranslation', (req, res) => {
   const {reqLanguage,data} = req.body
